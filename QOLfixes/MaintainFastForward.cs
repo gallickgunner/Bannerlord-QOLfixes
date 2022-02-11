d5da29c6db28e105c1b5c8cb6102275d6cb7b486
gallickgunner <gallick-gunner@hotmail.com>
2022-02-11 19:19:22 +0500

    upload project files

---
+++ QOLfixes/MaintainFastForward.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using SandBox.View.Map;
+
+namespace QOLfixes
+{
+    [HarmonyPatch]
+    public static class MaintainFastForward
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(MapScreen), "HandleMouse")]
+        public static IEnumerable<CodeInstruction> PatchHandleMouse(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo SetTimeControlModeMI = AccessTools.PropertySetter(typeof(Campaign), "TimeControlMode");
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].Calls(SetTimeControlModeMI))
+                {
+                    //Assignment to Campaign.Current.TimeControlMode
+                    codes[i].opcode = OpCodes.Nop;
+
+                    //ldc.i4.3 - set to NOP. This is the value of TimeControlMode.StoppablePlay
+                    codes[i - 1].opcode = OpCodes.Nop;
+
+                    // call to Campaign.Current
+                    codes[i - 2].opcode = OpCodes.Nop;
+
+                }
+            }
+            return codes.AsEnumerable();
+        }
+    }
+}
